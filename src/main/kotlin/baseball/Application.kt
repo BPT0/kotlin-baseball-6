c311c3b1a068311a11cdb8675f79409790e9c30e
Beyoung Su Kang <81085595+BPT0@users.noreply.github.com>
2023-10-20 22:36:45 +0900

    feat. 낫싱 및 개행 추가
    입력값이 다 일치 하지 않을떼 와 마지막 개행을 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -77,6 +77,12 @@ fun main() {
                 print("${strike}스트라이크")
             }
 
+            if (strike == 0 && ball == 0) {
+                println("낫싱")
+            }else{
+                println()
+            }
+
             if (strike == 3) {
                 println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
                 break

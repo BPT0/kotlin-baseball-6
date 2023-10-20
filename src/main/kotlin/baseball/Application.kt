d158630a47dab6030412c79bc8a4ef155c33673c
Beyoung Su Kang <81085595+BPT0@users.noreply.github.com>
2023-10-20 22:35:25 +0900

    feat 3스트라이크시 게임 재시작 루프로 탈출
    3스트라이크시 루프를 탈출하게 설정함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -77,6 +77,10 @@ fun main() {
                 print("${strike}스트라이크")
             }
 
+            if (strike == 3) {
+                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                break
+            }
         }
 
 
